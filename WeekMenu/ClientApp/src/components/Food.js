80a527363ce0227e03d4f4069251ae14aa6a5ccb
sindre.paulshus <sindre.paulshus@bouvet.no>
2021-04-30 16:00:59 +0200

    Progress

---
+++ WeekMenu/ClientApp/src/components/Food.js
@@ -0,0 +1,204 @@
+﻿export function MakeWeekPlan(dailycal, meals, fourthmeal) {
+    let dinners = meals.filter(meal => meal.types.includes(MealTypes.dinner));
+    let lunches = meals.filter(meal => meal.types.includes(MealTypes.lunch));
+    let breakfasts = meals.filter(meal => meal.types.includes(MealTypes.breakfast));
+    let suppers = meals.filter(meal => meal.types.includes(MealTypes.supper));
+
+    let dinneri = 0;
+
+    let plan = {
+        mon: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+        tue: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+        wed: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+        thurs: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+        friday: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+        saturday: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+        sunday: {
+            breakfast: [],
+            lunch: [],
+            dinner: [],
+            supper: []
+        },
+    }
+
+    Object.keys(plan).map((day) => {
+        let daycal = dailycal;
+
+        if (dinneri >= dinners.length) dinneri = 0;
+        plan[day].dinner = dinners[dinneri];
+        daycal -= GetMealCal(dinners[dinneri]);
+        dinneri++;
+
+        while (daycal > 0) {
+            let breakfasti = Math.floor(Math.random() * breakfasts.length);
+            plan[day].breakfast.push(breakfasts[breakfasti]);
+            daycal -= GetMealCal(breakfasts[breakfasti]);
+            if (daycal <= 0) break;
+
+            let lunchi = Math.floor(Math.random() * lunches.length);
+            plan[day].lunch.push(lunches[lunchi]);
+            daycal -= GetMealCal(lunches[lunchi]);
+            if (daycal <= 0) break;
+
+            let supperi = Math.floor(Math.random() * suppers.length);
+            plan[day].supper.push(suppers[supperi]);
+            daycal -= GetMealCal(suppers[supperi]);
+            if (daycal <= 0) break;
+        }
+    });
+
+    return plan;
+}
+
+export function GetMealCal(meal) {
+    let res = 0;
+    for (let i = 0; i < meal.ingredients.length; i++) {
+        if (meal.ingredients[i].food) {
+            res += meal.ingredients[i].food.cal / 100 * meal.ingredients[i].amountg;
+        }
+        else if (meal.ingredients[i].type) {
+            let foodoftype = [];
+            Object.keys(Foods).map((food) => {
+                if (Foods[food].type === meal.ingredients[i].type) {
+                    foodoftype.push(Foods[food]);
+                }
+            });
+            let randfood = Math.floor(Math.random() * foodoftype.length);
+            meal.ingredients[i].food = foodoftype[randfood];
+            res += meal.ingredients[i].food.cal / 100 * meal.ingredients[i].amountg;
+        }
+    }
+    return res;
+}
+
+export const FoodTypes = {
+    fruit: "fruit",
+    fiber: "fiber",
+    fibertopping: "fibertopping",
+    diary: "diary",
+    drink: "drink",
+    meat: "meat",
+    combo: "combo",
+    dinnerfiber: "dinnerfiber",
+    greens: "greens"
+}
+
+export const Foods = { //cal = per 100g
+    //fruits
+    banana: { name: "Banana", type: FoodTypes.fruit, cal: 83 },
+    apple: { name: "Apple", type: FoodTypes.fruit, cal: 100 },
+    orange: {
+        name: "Orange", type: FoodTypes.fruit, cal: 100 },
+
+    //fibers
+    breadslice: { name: "Breadslice", type: FoodTypes.fiber, cal: 100 },
+    baguette: { name: "Baguette", type: FoodTypes.fiber, cal: 100 },
+    muesli: { name: "Muesli", type: FoodTypes.fiber, cal: 100 },
+
+    //fiber toppings
+    cheese: { name: "Cheese", type: FoodTypes.fibertopping, cal: 100 },
+    salami: { name: "Salami", type: FoodTypes.fibertopping, cal: 100 },
+    brown_cheese: { name: "Brown Cheese", type: FoodTypes.fibertopping, cal: 100 },
+    strawberry_jam: { name: "Strawberry Jam", type: FoodTypes.fibertopping, cal: 100 },
+    nugatti_spread: { name: "Nugatti Spread", type: FoodTypes.fibertopping, cal: 100 },
+
+    //other
+    yoghurt: { name: "Yoghurt", type: FoodTypes.diary, cal: 100 },
+
+    //Drinks
+    milk: { name: "Milk", type: FoodTypes.drink, cal: 100 },
+    apple_juice: { name: "Apple Juice", type: FoodTypes.drink, cal: 100 },
+    orange_juice: { name: "Orange Juice", type: FoodTypes.drink, cal: 100 },
+    lemonade: { name: "Lemonade", type: FoodTypes.drink, cal: 100 },
+    soda: { name: "Soda", type: FoodTypes.drink, cal: 100 },
+
+    //Meats
+    minced_meat_cattle: { name: "Minced Meat (cattle)", type: FoodTypes.meat, cal: 100 },
+    minced_meat_pork: { name: "Minced Meat (pork)", type: FoodTypes.meat, cal: 100 },
+    chicken: { name: "Chicken", type: FoodTypes.meat, cal: 100 },
+    salmon: { name: "Salmon", type: FoodTypes.meat, cal: 100 },
+    trout: { name: "Trout", type: FoodTypes.meat, cal: 100 },
+    cod: { name: "Cod", type: FoodTypes.meat, cal: 100 },
+    sausage: { name: "Sausage", type: FoodTypes.meat, cal: 100 },
+
+    //Combo dinners
+    fish_gratin: { name: "Fish Gratin", type: FoodTypes.combo, cal: 100 },
+
+    //dinner fiber/carbs
+    pasta: { name: "Pasta", type: FoodTypes.dinnerfiber, cal: 100 },
+    potato: { name: "Potato", type: FoodTypes.dinnerfiber, cal: 100 },
+    rice: { name: "Rice", type: FoodTypes.dinnerfiber, cal: 100 },
+    tortilla: { name: "Tortilla", type: FoodTypes.dinnerfiber, cal: 100 },
+
+    //greens/veggies
+    carrot: { name: "Carrot", type: FoodTypes.greens, cal: 100 },
+    brokkoli: { name: "Brokkoli", type: FoodTypes.greens, cal: 100 },
+    peas: { name: "Peas", type: FoodTypes.greens, cal: 100 },
+    aspergus: { name: "Aspergus", type: FoodTypes.greens, cal: 100 },
+    aspergus_beans: { name: "(Aspergus) Beans", type: FoodTypes.greens, cal: 100 },
+    cucumber: { name: "Cucumber", type: FoodTypes.greens, cal: 100 },
+    corn: { name: "Corn", type: FoodTypes.greens, cal: 100 },
+};
+
+export const MealTypes = {
+    breakfast: "breakfast",
+    lunch: "lunch",
+    dinner: "dinner",
+    supper: "supper"
+}
+
+export const Meals = [
+    {
+        name: "Muesli and Yoghurt", types: [MealTypes.breakfast, MealTypes.lunch, MealTypes.supper], ingredients: [
+            { food: Foods.muesli, amountg: 100 },
+            { food: Foods.yoghurt, amountg: 100 }]
+    },
+    {
+        name: "Breadslice with toppings", types: [MealTypes.breakfast, MealTypes.lunch, MealTypes.supper], ingredients: [
+            { food: Foods.breadslice, amountg: 100 },
+            { food: null, amountg: 100, type: FoodTypes.fibertopping }]
+    },
+    {
+        name: "Baguette with toppings", types: [MealTypes.breakfast, MealTypes.lunch, MealTypes.supper], ingredients: [
+            { food: Foods.baguette, amountg: 100 },
+            { food: null, amountg: 100, type: FoodTypes.fibertopping}]
+    },
+    {
+        name: "Taco", types: [MealTypes.dinner], ingredients: [
+            { food: Foods.cheese, amountg: 100 },
+            { food: Foods.tortilla, amountg: 100 },
+            { food: Foods.minced_meat_cattle, amountg: 100 },
+            { food: Foods.cucumber, amountg: 100 },
+            { food: Foods.corn, amountg: 100 },]
+    },
+]
--- WeekMenu/ClientApp/src/components/Home.js
+++ WeekMenu/ClientApp/src/components/Home.js
@@ -1,26 +1,135 @@
 import React, { Component } from 'react';
+import { Button, Dropdown, ListGroup, Col, Row, Container } from 'react-bootstrap';
+import { FoodTypes, MealTypes, Foods, Meals, MakeWeekPlan } from './Food';
+
+import './Home.css';
+
+const womanCal = 2000;
+const manCal = 2500;
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
+    state = {
+        selectedfoodlist: [],
+        selectedcaloriesstr: "Calories",
+        selectedcalories: 0,
+        selectedfooddropdown: "Food",
+        fourthmeal: false,
+        weekplan: null
+    }
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    onChangeCalorieSetting = (e, n) => {
+        this.setState({
+            selectedcaloriesstr: e,
+            selectedcalories: n
+        });
+    }
+
+    onChangeFourthMeal = (e) => {
+        this.setState({
+            fourthmeal: !this.state.fourthmeal
+        });
+    }
+
+    onAddFood = (e) => {
+        let list = this.state.selectedfoodlist;
+        list.push(e);
+        this.setState({
+            selectedfoodlist: list
+        });
+    }
+    onRemoveFood = (e) => {
+        let list = this.state.selectedfoodlist;
+        list = list.filter(item => item.name !== e.name);
+        this.setState({
+            selectedfoodlist: list
+        });
+    }
+
+    calculatePlan = () => {
+        let plan = MakeWeekPlan(this.state.selectedcalories, this.state.selectedfoodlist, this.state.fourthmeal);
+        console.log(plan);
+        this.setState({
+            weekplan: plan
+        });
+    }
+
+    render () {
+        return (
+            <div id="container">
+                <h1>WeekMenu(tm)</h1>
+                <div id="calorieamount">
+                    <p>Pick gender / Calorie Amount</p>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            {this.state.selectedcaloriesstr}
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => this.onChangeCalorieSetting(`Man (${manCal})`, manCal)}>Man ({manCal})</Dropdown.Item>
+                            <Dropdown.Item onClick={() => this.onChangeCalorieSetting(`Woman (${womanCal})`, womanCal)}>Woman ({womanCal})</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div id="fourthmeal">
+                    <p>Include a 4th daily meal (Supper or evening/night meal)?</p>
+                    <input type="checkbox" checked={this.state.fourthmeal} onChange={this.onChangeFourthMeal} />
+                </div>
+                <div id="foodlist">
+                    <h2>Add your meals:</h2>
+                    <Container>
+                        {this.state.selectedfoodlist.map((item, i) =>
+                            <Row id="foodlistitem" key={i}>
+                                <Col>{item.name}</Col>
+                                <Col><Button variant="danger" onClick={() => this.onRemoveFood(item)}>-</Button></Col>
+                            </Row>
+                         )}
+                    </Container>
+                    <Container id="foodlistaddcontainer">
+                        <Row className="justify-content-md-center">
+                            <Col md="auto">
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    Add Food
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                        {Object.keys(Meals).map((prop, i) =>
+                                            <Dropdown.Item key={i} onClick={() => this.onAddFood(Meals[prop])}>{Meals[prop].name}</Dropdown.Item>
+                                    )}
+                                </Dropdown.Menu>
+                                </Dropdown>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div id="calculate">
+                    <Button variant="primary" onClick={this.calculatePlan}>Calculate</Button>
+                </div>
+                <div id="weekplan">
+                    {this.state.weekplan ? Object.keys(this.state.weekplan).map((day, i) => 
+                        <div key={i}>
+                            <p><b>{day}:</b></p>
+                            <p>Breakfast</p>
+                            {this.state.weekplan[day].breakfast.map((item, j) =>
+                                <p key={j}>-{item.name}</p>
+                            )}
+                            <p>Lunch</p>
+                            {this.state.weekplan[day].lunch.map((item, j) =>
+                                <p key={j}>-{item.name}</p>
+                            )}
+                            <p>Dinner</p>
+                            <p>-{this.state.weekplan[day].dinner.name}</p>
+                            <p>Supper</p>
+                            {this.state.weekplan[day].supper.map((item, j) =>
+                                <p key={j}>-{item.name}</p>
+                            )}
+                        </div>
+                    ) : <div/>
+                    }
+                </div>
+            </div>
+        );
+        //<Col md="auto"><Button variant="success">+</Button></Col>
+    }
 }
